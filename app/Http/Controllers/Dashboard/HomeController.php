976bf84ae4cdc205103bee7805c7bb4ca76337ab
Zulkifli Raihan <zuran2907@gmail.com>
2021-02-02 09:26:16 +0700

    Create per components

--- app/Http/Controllers/Dashboard/HomeController.php
+++ app/Http/Controllers/Dashboard/HomeController.php
@@ -9,6 +9,6 @@ class HomeController extends Controller
 {
     public function index()
     {
-        return view('dashboard.dashboard');
+        return view('dashboard.components.content');
     }
 }
