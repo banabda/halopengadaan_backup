09467632568e3b30527fd6179adb281af6d00e54
Bagas-Lpkn <banabda@gmail.com>
2021-02-10 16:43:36 +0700

    updated section six

--- resources/views/components/membership-card.blade.php
+++ resources/views/components/membership-card.blade.php
@@ -1,3 +1,13 @@
+<style>
+    .btn-daftar{
+        background: linear-gradient(to bottom, #3EC8AC, #4E90A4);
+        color: white;
+        border: none;
+    }
+    .btn-daftar:hover{
+        color: papayawhip;
+    }
+</style>
 <div class="linear-bg mx-auto py-4 px-3 text-center">
     <h3 class="my-5">{{ $title }}</h3>
     <h1 class="my-5">Rp {{ $harga }},-</h1>
@@ -7,5 +17,5 @@
         @endforeach
         
     </ul>
-    <button type="button" class="btn btn-warning my-5">DAFTAR SEKARANG</button>
+    <button type="button" class="btn btn-warning my-5 btn-daftar">DAFTAR SEKARANG</button>
 </div>
