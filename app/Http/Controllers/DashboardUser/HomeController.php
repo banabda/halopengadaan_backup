9db9e7742cf4c5fa41cf26693c8eaea180192692
Zulkifli Raihan <zuran2907@gmail.com>
2021-02-25 11:46:49 +0700

    punya dinda

--- app/Http/Controllers/DashboardUser/HomeController.php
+++ app/Http/Controllers/DashboardUser/HomeController.php
@@ -226,38 +226,40 @@ class HomeController extends Controller
 
     public function invoiceprofil()
     {
-      $data = Invoice::with('user')->where('user_id', Auth::user()->id)->get();
+        $data = Invoice::with('user')->where('user_id', Auth::user()->id)->get();
 
-      if(request()->ajax()){
-          return DataTables::of($data)
-            ->addColumn('total_tagihan', function($tagihan){
-                return 'Rp. '.number_format($tagihan->tagihan,0, ',', '.');
-            })
-            ->addColumn('tanggal', function($tanggal){
-                return Carbon::parse($tanggal->updated_at)->format('d F Y');
-            })
-            ->addColumn('paket_detail', function($paket){
-                if ($paket->paket == "1") {
-                    return 'Silver';
-                }
-                elseif ($paket->paket == "2") {
-                    return 'Gold';
-                }
-                else {
-                    return 'Platinum';
-                }
-            })
-            ->addColumn('action', function($action){
-                $btn = '<a class="btn btn-md btn-info mr-2" href="'. route("user.dashboard.cetak", $action->id) .'">
-            <i class="fa fa-print"></i>cetak</a>';
-            return $btn;
-            })
-            ->rawColumns(['total_tagihan', 'tanggal', 'paket_detail', 'action'])
-            ->addIndexColumn()
-            ->make(true);
-      }
+        if(request()->ajax()){
+            return DataTables::of($data)
+              ->addColumn('total_tagihan', function($tagihan){
+                  return 'Rp. '.number_format($tagihan->tagihan,0, ',', '.');
+              })
+              ->addColumn('tanggal', function($tanggal){
+                  return Carbon::parse($tanggal->updated_at)->format('d F Y');
+              })
+              ->addColumn('paket_detail', function($paket){
+                  if ($paket->paket == "1") {
+                      return 'Silver';
+                  }
+                  elseif ($paket->paket == "2") {
+                      return 'Gold';
+                  }
+                  else {
+                      return 'Platinum';
+                  }
+              })
+              ->addColumn('action', function($action){
+                  $btn = '<a class="btn btn-md btn-info mr-2" href="'. route("user.dashboard.cetak", $action->id) .'">
+              <i class="fa fa-print"></i>cetak</a>';
+              return $btn;
+              })
+              ->rawColumns(['total_tagihan', 'tanggal', 'paket_detail', 'action'])
+              ->addIndexColumn()
+              ->make(true);
+        }
 
-      return view('dashboard.user.invoiceprofil');
+        return view('dashboard.user.invoiceprofil');
+
+
     }
 
     public function konsultasi()
