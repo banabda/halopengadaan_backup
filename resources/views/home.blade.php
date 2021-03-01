ac50043cac9495e0c6d4c3d3069d5ed258cdb309
Bagas-Lpkn <banabda@gmail.com>
2021-03-01 13:14:46 +0700

    added navbar animation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<div class="section-one" id="section-one" style="background-image: url('{{ asset('images/back-sect-1.png') }}'); background-size: 1200px">
+<div class="section-one" id="section-one" style="background-image: url('{{ asset('images/back-sect-1.png') }}');">
     @include('components.navbar')
     <div class="container section-one-content">
         <div class="row justify-content-center">
@@ -75,53 +75,74 @@
     window.addEventListener("scroll", () => {
     const currentScroll = window.pageYOffset;
     console.log(currentScroll, opacity)
-    if (currentScroll >= 550 && opacity[0] == 0) {
-        opacity[0] = 1;
-        document.querySelector(".two-tittle").style.opacity = 1;
-        document.querySelector(".two-btn-1").style.opacity = 1;
-        document.querySelector(".two-btn-2").style.opacity = 1;
-        document.querySelector(".two-btn-3").style.opacity = 1;
-        document.querySelector(".two-btn-4").style.opacity = 1;
-        document.querySelector(".two-btn-5").style.opacity = 1;
-        document.querySelector(".two-btn-6").style.opacity = 1;
-        document.querySelector(".two-btn-7").style.opacity = 1;
+
+    if (currentScroll < 550) {
+        document.querySelector(".navbar").style.backgroundColor = "#f5d00000";
+        
     }
 
-    if (currentScroll >= 1200 && opacity[1] == 0) {
+    if (currentScroll >= 550) {
+        if (opacity[0] == 0) {
+            opacity[0] = 1;
+            document.querySelector(".two-tittle").style.opacity = 1;
+            document.querySelector(".two-btn-1").style.opacity = 1;
+            document.querySelector(".two-btn-2").style.opacity = 1;
+            document.querySelector(".two-btn-3").style.opacity = 1;
+            document.querySelector(".two-btn-4").style.opacity = 1;
+            document.querySelector(".two-btn-5").style.opacity = 1;
+            document.querySelector(".two-btn-6").style.opacity = 1;
+            document.querySelector(".two-btn-7").style.opacity = 1;
+        }
+        document.querySelector(".navbar").style.backgroundColor = "#f5d000";
+    }
+
+    if (currentScroll >= 1200) {
+        if (opacity[1] == 0) {
         opacity[1] = 1;
         document.querySelector(".three-img").style.opacity = 1;
         document.querySelector(".three-p-2").style.opacity = 1;
         document.querySelector(".three-p-1").style.opacity = 1;
+        }
+        document.querySelector(".navbar").style.backgroundColor = "#f5d000";
     }
 
-    if (currentScroll >= 1900 && opacity[2] == 0) {
-        opacity[2] = 1;
-        document.querySelector(".four-tittle").style.opacity = 1;
-        document.querySelector(".four-btn-1").style.opacity = 1;
-        document.querySelector(".four-btn-2").style.opacity = 1;
-        document.querySelector(".four-btn-3").style.opacity = 1;
-        document.querySelector(".four-btn-4").style.opacity = 1;
-        document.querySelector(".four-btn-5").style.opacity = 1;
-        document.querySelector(".four-btn-6").style.opacity = 1;
-        document.querySelector(".four-btn-7").style.opacity = 1;
+    if (currentScroll >= 1900) {
+        if (opacity[2] == 0) {
+            opacity[2] = 1;
+            document.querySelector(".four-tittle").style.opacity = 1;
+            document.querySelector(".four-btn-1").style.opacity = 1;
+            document.querySelector(".four-btn-2").style.opacity = 1;
+            document.querySelector(".four-btn-3").style.opacity = 1;
+            document.querySelector(".four-btn-4").style.opacity = 1;
+            document.querySelector(".four-btn-5").style.opacity = 1;
+            document.querySelector(".four-btn-6").style.opacity = 1;
+            document.querySelector(".four-btn-7").style.opacity = 1;
+        }
+        document.querySelector(".navbar").style.backgroundColor = "#4E90A4";
     }
 
-    if (currentScroll >= 2400 && opacity[3] == 0) {
-        opacity[3] = 1;
-        document.querySelector(".five-tittle").style.opacity = 1;
-        document.querySelector(".five-btn-1").style.opacity = 1;
-        document.querySelector(".five-btn-2").style.opacity = 1;
-        document.querySelector(".five-btn-3").style.opacity = 1;
-        document.querySelector(".five-btn-4").style.opacity = 1;
-        document.querySelector(".five-btn-5").style.opacity = 1;
+    if (currentScroll >= 2400) {
+        if (opacity[3] == 0) {
+            opacity[3] = 1;
+            document.querySelector(".five-tittle").style.opacity = 1;
+            document.querySelector(".five-btn-1").style.opacity = 1;
+            document.querySelector(".five-btn-2").style.opacity = 1;
+            document.querySelector(".five-btn-3").style.opacity = 1;
+            document.querySelector(".five-btn-4").style.opacity = 1;
+            document.querySelector(".five-btn-5").style.opacity = 1;
+        }
+        document.querySelector(".navbar").style.backgroundColor = "#f5d000";
     }
 
-    if (currentScroll >= 3200 && opacity[4] == 0) {
-        opacity[4] = 1;
-        document.querySelector(".six-tittle").style.opacity = 1;
-        document.querySelector(".six-btn-1").style.opacity = 1;
-        document.querySelector(".six-btn-2").style.opacity = 1;
-        document.querySelector(".six-btn-3").style.opacity = 1;
+    if (currentScroll >= 3200) {
+        if (opacity[4] == 0) {
+            opacity[4] = 1;
+            document.querySelector(".six-tittle").style.opacity = 1;
+            document.querySelector(".six-btn-1").style.opacity = 1;
+            document.querySelector(".six-btn-2").style.opacity = 1;
+            document.querySelector(".six-btn-3").style.opacity = 1;
+        }
+        document.querySelector(".navbar").style.backgroundColor = "#3EC8AC";
     }
     });
 </script>
