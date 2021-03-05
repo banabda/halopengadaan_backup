077b85c176ecd2ccfde9f4100b72072b9ebeeeaa
Bagas-Lpkn <banabda@gmail.com>
2021-03-05 15:43:07 +0700

    update section six membership title

--- resources/views/components/membership-card.blade.php
+++ resources/views/components/membership-card.blade.php
@@ -15,17 +15,24 @@
     }
 </style>
 <div class="linear-bg mx-auto py-4 px-3 text-center">
-    <h3 class="my-5">{{ $title }}</h3>
-    <h1 class="my-5">Rp {{ $harga }},-</h1>
-    <ul class="text-left">
-        @foreach ($point as $item)
-        <li>{{ $item }}</li>
-        @endforeach
+    <div class="h-25">
+        <h2>{{ $title }}</h2>
+    </div>
+    <div class="h-25">
+        <h1>Rp {{ $harga }},-</h1>
+    </div>
+    <div class="h-50">
+        <ul class="text-left">
+            @foreach ($point as $item)
+            <li><h6 class="py-1">{{ $item }}</h6></li>
+            @endforeach
 
-    </ul>
-    <form class="form-button" action="{{ route('landing.paket', $id) }}" method="POST">
-        @csrf
-        <input type="text" name="paket" id="" value="{{ $id }}" hidden>
-        <button type="submit" class="btn btn-warning btn-daftar">DAFTAR SEKARANG</button>
-    </form>
+        </ul>
+        <form class="form-button" action="{{ route('landing.paket', $id) }}" method="POST">
+            @csrf
+            <input type="text" name="paket" id="" value="{{ $id }}" hidden>
+            <button type="submit" class="btn btn-warning btn-daftar">DAFTAR SEKARANG</button>
+        </form>
+    </div>
+    
 </div>
--- resources/views/components/section-six.blade.php
+++ resources/views/components/section-six.blade.php
@@ -38,13 +38,13 @@
                 </div>
                 <div class="row justify-content-center mb-md-4">
                     <div class="col-md-4 col-jenis six-btn-1">
-                        @include('components.membership-card', ['title'=>'Silver', 'harga'=>'250.000,- / Bulan', 'point' => ['Ditangani para ahli','Sumber jawaban terkelola','Fast response','Identitas terjaga'], 'id' => 1])
+                        @include('components.membership-card', ['title'=>'konsultasi persesi / 30 menit', 'harga'=>'50.000,- / Bulan', 'point' => ['Ditangani para ahli','Sumber jawaban terkelola','Fast response','Identitas terjaga'], 'id' => 1])
                     </div>
                     <div class="col-md-4 col-jenis six-btn-2 my-3 my-md-0">
-                        @include('components.membership-card', ['title'=>'Gold', 'harga'=>'600.000,- / 3 Bulan', 'point' => ['Ditangani para ahli','Sumber jawaban terkelola','Fast response','Identitas terjaga', 'Lebih murah'], 'id' => 2])
+                        @include('components.membership-card', ['title'=>'Bulanan', 'harga'=>'250.000,- / 3 Bulan', 'point' => ['Ditangani para ahli','Sumber jawaban terkelola','Fast response','Identitas terjaga', 'Lebih murah'], 'id' => 2])
                     </div>
                     <div class="col-md-4 col-jenis six-btn-3">
-                        @include('components.membership-card', ['title'=>'Platinum', 'harga'=>'1.500.000,- / 1 Jam', 'point' => ['Ditangani para ahli','Sumber jawaban terkelola','Fast response','Identitas terjaga', 'Konsultasi via Zoom'], 'id' => 3])
+                        @include('components.membership-card', ['title'=>'Konsultasi Via zoom', 'harga'=>'1.500.000,- / 1 Jam', 'point' => ['Ditangani para ahli','Sumber jawaban terkelola','Fast response','Identitas terjaga', 'Konsultasi via Zoom'], 'id' => 3])
                     </div>
                 </div>
             </div>
