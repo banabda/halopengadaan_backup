3a030f3213cd11475d13f31a06ed5417e1e67c58
Bagas-Lpkn <banabda@gmail.com>
2021-03-18 16:48:10 +0700

    fixed section four mobile design font size

--- resources/views/components/section-four.blade.php
+++ resources/views/components/section-four.blade.php
@@ -6,35 +6,35 @@
                     Kelebihan Layanan
                 </h1>
                 <div class="row justify-content-center mb-3 px-5">
-                    <div class="col-md-3 col-xs-6 divkelebihan four-btn-1">
+                    <div class="col-md-3 divkelebihan four-btn-1">
                         <img class="mx-auto kelebihanimg h-75" src="{{ asset('images/kelebihan/pro.svg')}}" alt="ahli">
                         <p class="text-center section-content-paragraph h-25">Ditangani Para Ahli Yang Berpengalaman</p>
                     </div>
-                    <div class="col-md-3 col-xs-6 divkelebihan four-btn-2">
+                    <div class="col-md-3 divkelebihan four-btn-2">
                         <img src="{{ asset('images/kelebihan/tim.svg')}}" class="mx-auto kelebihanimg h-75" alt="ahli">
                         <p class="text-center section-content-paragraph h-25">Sumber Jawaban Dikelola Oleh Tim</p>
                     </div>
-                    <div class="col-md-3 col-xs-6 divkelebihan four-btn-3 p-1">
+                    <div class="col-md-3 divkelebihan four-btn-3 p-1">
                         {{-- <div class="pt-3" style="background-color: grey; border-radius: 10%; height: 250px; width: 250px">
                         </div> --}}
                         <img src="{{ asset('images/kelebihan/chatonline.svg')}}" class="mx-auto kelebihanimg h-75" alt="ahli">
                         <p class="text-center section-content-paragraph h-25">Konsultasi Online</p>
                     </div>
-                    <div class="col-md-3 col-xs-6 divkelebihan four-btn-4">
+                    <div class="col-md-3 divkelebihan four-btn-4">
                         <img src="{{ asset('images/kelebihan/waktu.svg')}}" class="mx-auto kelebihanimg h-75" alt="ahli">
                         <p class="text-center section-content-paragraph h-25">Hemat waktu</p>
                     </div>
                 </div>
                 <div class="row-md justify-content-center">
-                    <div class="col-md-3 col-xs-6 divkelebihan four-btn-5">
+                    <div class="col-md-3 divkelebihan four-btn-5">
                         <img src="{{ asset('images/kelebihan/biaya.svg')}}" class="mx-auto kelebihanimg h-75" alt="ahli">
                         <p class="text-center section-content-paragraph h-25">Hemat biaya</p>
                     </div>
-                    <div class="col-md-3 col-xs-6 divkelebihan four-btn-6">
+                    <div class="col-md-3 divkelebihan four-btn-6">
                         <img src="{{ asset('images/kelebihan/cepat.svg')}}" class="mx-auto kelebihanimg h-75" alt="ahli">
                         <p class="text-center section-content-paragraph h-25">Respon yang cepat</p>
                     </div>
-                    <div class="col-md-3 col-xs-6 divkelebihan four-btn-7">
+                    <div class="col-md-3 divkelebihan four-btn-7">
                         <img src="{{ asset('images/kelebihan/privasi.svg')}}" class="mx-auto kelebihanimg h-75" alt="ahli">
                         <p class="text-center section-content-paragraph h-25">Identitas terjaga</p>
                     </div>
