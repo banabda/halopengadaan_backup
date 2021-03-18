64d7e8ed17e53366ce97b246307fcdc7e400d46e
Zulkifli Raihan <zuran2907@gmail.com>
2021-03-18 15:57:02 +0700

    change row and artikel

--- resources/views/dashboard/admin/artikel/create.blade.php
+++ resources/views/dashboard/admin/artikel/create.blade.php
@@ -51,7 +51,7 @@
                             </div>
                             <div class="form-group">
                                 <label>Konten Artikel</label>
-                                <textarea name="desc" class="form-control editor" placeholder="Textarea text"></textarea>
+                                <textarea name="desc" class="form-control editor" rows="20" placeholder="Textarea text"></textarea>
                             </div>
                             <div class="form-group">
                                 <label>Foto Artikel</label>
