266693493209e7bd962ed2a6b60ed328c7fe49b0
Bagas-Lpkn <banabda@gmail.com>
2021-03-18 16:42:28 +0700

    fixed section four mobile design

--- resources/views/components/section-four.blade.php
+++ resources/views/components/section-four.blade.php
@@ -7,35 +7,35 @@
                 </h1>
                 <div class="row justify-content-center mb-3 px-5">
                     <div class="col-md-3 col-xs-6 divkelebihan four-btn-1">
-                        <img class="mx-auto kelebihanimg h-75" src="{{ asset('images/kelebihan/pro.svg')}}" style="display: block"  width="100%" alt="ahli">
+                        <img class="mx-auto kelebihanimg h-75" src="{{ asset('images/kelebihan/pro.svg')}}" alt="ahli">
                         <p class="text-center section-content-paragraph h-25">Ditangani Para Ahli Yang Berpengalaman</p>
                     </div>
                     <div class="col-md-3 col-xs-6 divkelebihan four-btn-2">
-                        <img src="{{ asset('images/kelebihan/tim.svg')}}"  style="display: block"  width="100%" class="mx-auto kelebihanimg h-75" alt="ahli">
+                        <img src="{{ asset('images/kelebihan/tim.svg')}}" class="mx-auto kelebihanimg h-75" alt="ahli">
                         <p class="text-center section-content-paragraph h-25">Sumber Jawaban Dikelola Oleh Tim</p>
                     </div>
                     <div class="col-md-3 col-xs-6 divkelebihan four-btn-3 p-1">
                         {{-- <div class="pt-3" style="background-color: grey; border-radius: 10%; height: 250px; width: 250px">
                         </div> --}}
-                        <img src="{{ asset('images/kelebihan/chatonline.svg')}}"  style="display: block"  width="100%" class="mx-auto kelebihanimg h-75" alt="ahli">
+                        <img src="{{ asset('images/kelebihan/chatonline.svg')}}" class="mx-auto kelebihanimg h-75" alt="ahli">
                         <p class="text-center section-content-paragraph h-25">Konsultasi Online</p>
                     </div>
                     <div class="col-md-3 col-xs-6 divkelebihan four-btn-4">
-                        <img src="{{ asset('images/kelebihan/waktu.svg')}}"  style="display: block"  width="100%" class="mx-auto kelebihanimg h-75" alt="ahli">
+                        <img src="{{ asset('images/kelebihan/waktu.svg')}}" class="mx-auto kelebihanimg h-75" alt="ahli">
                         <p class="text-center section-content-paragraph h-25">Hemat waktu</p>
                     </div>
                 </div>
-                <div class="row justify-content-center">
+                <div class="row-md justify-content-center">
                     <div class="col-md-3 col-xs-6 divkelebihan four-btn-5">
-                        <img src="{{ asset('images/kelebihan/biaya.svg')}}"  style="display: block"  width="100%" class="mx-auto kelebihanimg h-75" alt="ahli">
+                        <img src="{{ asset('images/kelebihan/biaya.svg')}}" class="mx-auto kelebihanimg h-75" alt="ahli">
                         <p class="text-center section-content-paragraph h-25">Hemat biaya</p>
                     </div>
                     <div class="col-md-3 col-xs-6 divkelebihan four-btn-6">
-                        <img src="{{ asset('images/kelebihan/cepat.svg')}}"  style="display: block"  width="100%" class="mx-auto kelebihanimg h-75" alt="ahli">
+                        <img src="{{ asset('images/kelebihan/cepat.svg')}}" class="mx-auto kelebihanimg h-75" alt="ahli">
                         <p class="text-center section-content-paragraph h-25">Respon yang cepat</p>
                     </div>
                     <div class="col-md-3 col-xs-6 divkelebihan four-btn-7">
-                        <img src="{{ asset('images/kelebihan/privasi.svg')}}"  style="display: block"  width="100%" class="mx-auto kelebihanimg h-75" alt="ahli">
+                        <img src="{{ asset('images/kelebihan/privasi.svg')}}" class="mx-auto kelebihanimg h-75" alt="ahli">
                         <p class="text-center section-content-paragraph h-25">Identitas terjaga</p>
                     </div>
                 </div>
