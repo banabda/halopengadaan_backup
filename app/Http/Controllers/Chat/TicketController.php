74fd2bdd2dc8764824ca04f2fb5ffc9db33df590
Bagas-Lpkn <banabda@gmail.com>
2021-03-25 09:37:43 +0700

    updated time chat to 30 minutes

--- app/Http/Controllers/Chat/TicketController.php
+++ app/Http/Controllers/Chat/TicketController.php
@@ -24,7 +24,7 @@ class TicketController extends Controller
         $ticket = Ticket::create([
             'name' => $name,
             'room_id' => $request->id,
-            'expired_at' => Carbon::now()->addMinutes(60)
+            'expired_at' => Carbon::now()->addMinutes(30)
         ]);
         $room = Room::find($request->id);
         $room->ticket = $name;
