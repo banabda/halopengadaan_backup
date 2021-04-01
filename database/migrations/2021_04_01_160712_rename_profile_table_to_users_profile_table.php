5b727d1651c3e8562d967dfac270ca9f6c6976ec
Zulkifli Raihan <zuran2907@gmail.com>
2021-04-01 16:37:27 +0700

    Change Name Table and Drop Column Alamat Kerja dan Alamat Rumah

---
+++ database/migrations/2021_04_01_160712_rename_profile_table_to_users_profile_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RenameProfileTableToUsersProfileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::rename('profile', 'users_profile');
+        Schema::table('users_profile', function (Blueprint $table) {
+            $table->dropColumn('alamat_kerja');
+            $table->dropColumn('alamat_rumah');
+            $table->dropColumn('alamat_kerja');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::rename('profile', 'users_profile');
+        Schema::table('users_profile', function (Blueprint $table) {
+            $table->dropColumn('alamat_kerja');
+            $table->dropColumn('alamat_rumah');
+            $table->dropColumn('alamat_kerja');
+        });
+    }
+}
