59d713fff0407bea2d6cc254e0c2389388a64cb1
dindawulandari <Wdinda375@gmail.com>
2021-04-21 16:57:04 +0700

    section-eight

--- resources/views/components/section-eight.blade.php
+++ resources/views/components/section-eight.blade.php
@@ -1,3 +1,6 @@
+<div class="section-eight">
 <div class="container" style="margin-bottom: 50px; margin-top: -50px">
     <img src="{{ asset('images/infografis-pendaftaran.jpg') }}" alt="" width="100%" srcset="">
 </div>
+</div>
+
